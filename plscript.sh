07066ffe96807b09c4ebf4b125cecee9f789dfd9
fish-hue <65360079+fish-hue@users.noreply.github.com>
2020-09-05 11:22:43 -0500

    execute failed.

--- plscript.sh
+++ plscript.sh
@@ -3,12 +3,6 @@
 # Ping bash loop script
 # substitute ip range and address accordingly
 
-git add --chmod=+x plscript.sh 
-# Flag it as executable
-git commit -m "Make plscript.sh executable."
-# Push the commit
-git push
-
 for ip in $(seq 0 254);
 do
 	ping -c 1 -W 2 10.10.10.$ip;
